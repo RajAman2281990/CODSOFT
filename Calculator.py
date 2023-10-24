ea30067b6f16db1d0135a6d9b12334b2e1d7deae
RajAman2281990 <148772560+RajAman2281990@users.noreply.github.com>
2023-10-24 19:53:34 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,12 @@
 def add(x, y):
     return x + y
+
 def subtract(x, y):
     return x - y
+
 def multiply(x, y):
     return x * y
+
 def divide(x, y):
     if y == 0:
         return "Cannot divide by zero"
